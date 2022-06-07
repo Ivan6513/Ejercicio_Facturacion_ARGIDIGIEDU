33d45f42ce59fa6b42911e7ea18a2f4874700272
ivan <ivanmarmilich@gmail.com>
2022-06-07 19:04:00 -0300

    Se actualizo el ejercicio

--- src/Facturacion.java
+++ src/Facturacion.java
@@ -3,28 +3,29 @@ import java.util.Scanner;
 public class Facturacion {
 
 	public static void main(String[] args) {
-		System.out.println("*************************************\n" 
-	                           + "******* Bienvenido ARGDIGIEDU *******\n"
-				   + "*************************************\n");
+		
+		System.out.println("*************************************\n"
+		                 + "******* Bienvenido ARGDIGIEDU *******\n"
+				         + "*************************************\n");
 
 		Scanner teclado = new Scanner(System.in);
-
-// Ingreso por teclado
+		
+		// Ingreso por teclado
 		System.out.print("Ingrese prefijo de factura: ");
 		String prefijoFactura = teclado.nextLine();
 
 		teclado.close();
-
-// Inicializacion de variables
+		
+		// Inicializacion de variables
 		int numeroAleatorio = 0;
 		final int LIMITE = 20;
 		String caracterNumeral = "#";
 
 		int numeroCaracteresIngresados = prefijoFactura.length();
 		int diferencia = LIMITE - numeroCaracteresIngresados;
-
+		
 		if (diferencia > 0) {
-
+			
 			// Establecemos un rango, este es un numero aleatorio que va desde 0 hasta el
 			// valor obtenido de la diferencia + 1
 			int rango = (int) (Math.random() * (diferencia) + 1);
@@ -36,12 +37,12 @@ public class Facturacion {
 			int digitosNumeroAleatorio = Integer.toString(numeroAleatorio).length();
 
 			// Obtenemos el numero de repeticiones por diferencia
-			// Donde repeticiones = LIMITE - numeroCaracteresIngresados -
-			// digitosNumeroAleatorio
+			// Donde repeticiones = LIMITE - numeroCaracteresIngresados - digitosNumeroAleatorio
 			int repeticiones = diferencia - digitosNumeroAleatorio;
 
 			String codigoFactura = prefijoFactura.toUpperCase() + caracterNumeral.repeat(repeticiones)
 					+ numeroAleatorio;
+			
 			System.out.println("El numero de factura generado es: " + codigoFactura);
 
 		} else {
